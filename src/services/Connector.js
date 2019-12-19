494b799df7139f4920f97d2ae893e54ba85e849a
Ilya Lebedev <bacherko@gmail.com>
2019-12-19 12:24:56 +0300

    refactor: update notifications

--- src/services/Connector.js
+++ src/services/Connector.js
@@ -60,7 +60,12 @@ class Connector extends BasicConnector {
                     before: [this._checkAuth],
                 },
 
-                'notify.getNotifications': this._authProxyTo('notify', 'getNotifications'),
+                'notifications.getNotifications': this._authProxyTo(
+                    'notifications',
+                    'getNotifications'
+                ),
+                'notifications.markAllAsSeen': this._authProxyTo('notifications', 'markAllAsSeen'),
+                'notifications.markAsRead': this._authProxyTo('notifications', 'markAsRead'),
 
                 'favorites.get': {
                     handler: options.getFavorites,
@@ -224,7 +229,7 @@ class Connector extends BasicConnector {
             requiredClients: {
                 frontend: env.GLS_FRONTEND_GATE_CONNECT,
                 onlineNotify: env.GLS_ONLINE_NOTIFY_CONNECT,
-                notify: env.GLS_NOTIFY_CONNECT,
+                notifications: env.GLS_NOTIFICATIONS_CONNECT,
                 options: env.GLS_OPTIONS_CONNECT,
                 push: env.GLS_PUSH_CONNECT,
                 mail: env.GLS_MAIL_CONNECT,
