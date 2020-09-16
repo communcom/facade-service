c9e4ceb97956f15ba03911bf228cddc9b5c3c846
Alexey Elizarov <felex-ae@ya.ru>
2020-09-16 15:16:38 +0300

    feat: trending tags

--- src/services/Connector.js
+++ src/services/Connector.js
@@ -159,6 +159,7 @@ class Connector extends BasicConnector {
                 'content.getReportsList': content.createCallProxy('getReportsList'),
                 'content.getEntityReports': content.createCallProxy('getEntityReports'),
                 'content.getBanPostProposal': content.createCallProxy('getBanPostProposal'),
+                'content.getTrendingTags': content.createCallProxy('getTrendingTags'),
                 'content.getReferralUsers': this._authProxyTo('registration', 'getReferralUsers'),
 
                 'meta.getPostsViewCount': {
